0e9c013a8d7cb6191b18d9e1dce86da6ffd06cbd
Kostis Gourgoulias <kgourgoulias@gmail.com>
2024-05-06 13:36:36 +0100

    update requirements

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import find_packages, setup
 
+# load requirements
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
 setup(
-    name='src',
+    name="src",
     packages=find_packages(),
-    version='0.1.0',
-    description='A short description of the project.',
-    author='Kostis Gourgoulias',
-    license='',
+    version="0.1.0",
+    description="A short description of the project.",
+    author="Kostis Gourgoulias",
+    license="",
+    install_requires=requirements,
+    setup_requires=["setuptools"],
 )
